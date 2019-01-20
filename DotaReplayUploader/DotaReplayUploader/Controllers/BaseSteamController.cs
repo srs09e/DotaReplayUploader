000d5ea9ec8f1643f66f91b5dd4466d3c1fb70db
srs09e <sirsmitty91@yahoo.com>
2019-01-20 16:06:39 -0700

    Update BaseSteamController.cs

--- DotaReplayUploader/DotaReplayUploader/Controllers/BaseSteamController.cs
+++ DotaReplayUploader/DotaReplayUploader/Controllers/BaseSteamController.cs
@@ -7,8 +7,8 @@ namespace DotaReplayUploader.Controllers
     {
         protected static HttpClient client = new HttpClient();
 
-        protected const string ApiKey = "6A6D20F570231ED182B34AEDFF1DACD8";
-        protected const string MyId = "76561197975498171";
+        protected const string ApiKey = "";
+        protected const string MyId = "";
         protected const string GetAllHeroesEndpoint = "https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v1/?key=" + ApiKey;
         protected const string ResolveVanityUrlEndpoint = "https://api.steampowered.com/ISteamUser/ResolveVanityURL/v1/?key=" + ApiKey;
         protected const string GetMatchHistoryEndpoint = "https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v1/?key=" + ApiKey;
